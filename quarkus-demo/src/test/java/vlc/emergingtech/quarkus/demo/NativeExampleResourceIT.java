caafea7b7d035bcdb65d4e722825a22cb2afeb48
jzuriaga <jzuriaga@redhat.com>
2019-11-25 14:42:37 +0100

    Sources from code.quarkus.io.

---
+++ quarkus-demo/src/test/java/vlc/emergingtech/quarkus/demo/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package vlc.emergingtech.quarkus.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
